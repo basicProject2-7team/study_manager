09e8f783caaf1baac2a9a39f714b18ccca4ed732
parkjunyeap <bab85503@gmail.com>
2023-12-10 11:33:19 +0900

    Revert "몽고디비 연동 아직"
    This reverts commit a60684c5cf8f0847001d54803ceec236bad9736b.

--- study_manager/src/main/java/module-info.java
+++ study_manager/src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.sm.study_manager {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
-    requires org.mongodb.driver.sync.client;
 
     opens com.sm.study_manager to javafx.fxml;
     exports com.sm.study_manager;
